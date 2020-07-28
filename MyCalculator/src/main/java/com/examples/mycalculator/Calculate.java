227346574da35aecc9729e725c326edd70cef652
zeeshanashraf <zeeshanashraf772@gmail.com>
2020-07-28 11:30:57 +0500

    Secound

--- MyCalculator/src/main/java/com/examples/mycalculator/Calculate.java
+++ MyCalculator/src/main/java/com/examples/mycalculator/Calculate.java
@@ -5,9 +5,19 @@ import android.widget.Toast;
 
 public class Calculate {
 
-    public static void Show(Context context,String message)
+    public static int Add(Context context,int first,int secound)
     {
-        Toast.makeText(context, message+"", Toast.LENGTH_SHORT).show();
+        return first+secound;
+    }
+
+    public static int Subtract(Context context,int first,int secound)
+    {
+        return first-secound;
+    }
+
+    public static int Multiply(Context context,int first,int secound)
+    {
+        return first*secound;
     }
 
 }
--- app/src/main/java/com/example/testingsdks/MainActivity.java
+++ app/src/main/java/com/example/testingsdks/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.examples.mycalculator.Calculate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calculate.Show(this,"Hello Friends");
     }
 }
